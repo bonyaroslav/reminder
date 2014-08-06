a2530b240fa3f46c4b18a1aa8984263015c36b9d
sev1 <bonyaroslav@gmail.com>
2014-08-06 14:46:20 +0300

    Added controls

---
+++ Fligern.UsefulReminder/Fligern.UsefulReminder.Base/ReturnData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fligern.UsefulReminder.Base
+{
+    public class ReturnData
+    {
+        public string Data { get; set; }
+        public int Number { get; set; }
+        public ReturnData()
+        {
+            Number = -1;
+        }
+    }
+}
---
+++ Fligern.UsefulReminder/Fligern.UsefulReminder/ApplicationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Fligern.UsefulReminder
+{
+    sealed class ApplicationController
+    {
+        MainViewModel viewModel;
+        MainWindow window;
+        Window ownedWindow;
+        public void Initialize()
+        {
+            viewModel = new MainViewModel();            
+           
+            window= new MainWindow();
+            window.DataContext = viewModel;
+
+            Fligern.UsefulReminder.Controls.QuotePopup popup = new Controls.QuotePopup();
+            popup.DataContext = viewModel;
+
+            ownedWindow = new Window();
+            //ownedWindow.Owner = window;
+            ownedWindow.Content = popup;
+
+            window.ChildWindow = ownedWindow;
+            window.Show();
+
+            
+        }
+    }
+}
